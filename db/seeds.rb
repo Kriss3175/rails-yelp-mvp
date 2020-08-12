ac7937bcb0849aa82784c00877ca93e4bbae0be3
christel dufrasne <chbalsacq@gmail.com>
2020-08-12 13:09:16 +0200

    yelp frist step

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+restaurant1 = Restaurant.create!(name: 'Le Rocher', address: 'Rue du Nord Grand Baie', category: 'italian')
+restaurant2 = Restaurant.create!(name: 'Supriz', address: 'Oberkamp', category: 'french' )
+restaurant3 = Restaurant.create!(name: 'Le Servan', address: 'Rue des Capucines', category: 'french' )
+restaurant4 = Restaurant.create!(name: 'Frite House', address: 'Champ des Reliefs', category: 'belgian' )
+restaurant5 = Restaurant.create!(name: 'Mario', address: 'Boulevard de la merveille', category: 'italian')
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
